c9165d419675e41241a9827927af3e41ebcff624
Brian Silah <unpervertedkid@gmail.com>
2023-09-20 15:35:50 +0300

    Defining and using structs

--- rustlang_book/structs/src/main.rs
+++ rustlang_book/structs/src/main.rs
@@ -1,4 +1,33 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.height * self.width;
+    }
+
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        return self.height > rectangle.height && self.width > rectangle.width;
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
