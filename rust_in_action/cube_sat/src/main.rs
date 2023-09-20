5bb5a9c6c36de2553386ef18bf02e91d69b9b421
Brian Silah <unpervertedkid@gmail.com>
2023-09-20 15:53:52 +0300

    Borrowing a cube sat to check its status instead of owning it

--- rust_in_action/cube_sat/src/main.rs
+++ rust_in_action/cube_sat/src/main.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 struct CubeSat {
-    id: u64
+    id: u64,
 }
 
 #[derive(Debug)]
@@ -8,23 +8,23 @@ enum StatusMessage {
     Ok,
 }
 
-fn check_status(cube_sat: CubeSat) -> StatusMessage {
+fn check_status(cube_sat: &CubeSat) -> StatusMessage {
     return StatusMessage::Ok;
 }
 
 fn main() {
-    let sat_a = CubeSat{ id: 0 };
-    let sat_b = CubeSat{ id: 1 };
-    let sat_c = CubeSat{ id: 2 };
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
 
-    let a_status = check_status(sat_a);
-  let b_status = check_status(sat_b);
-  let c_status = check_status(sat_c);
-  println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    let a_status = check_status(&sat_a);
+    let b_status = check_status(&sat_b);
+    let c_status = check_status(&sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
-  // "waiting" ...
-  let a_status = check_status(sat_a);
-  let b_status = check_status(sat_b);
-  let c_status = check_status(sat_c);
-  println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    // "waiting" ...
+    let a_status = check_status(&sat_a);
+    let b_status = check_status(&sat_b);
+    let c_status = check_status(&sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 }
