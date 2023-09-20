bbfca4b0423a80d0f51ea56146268ea5255d3c91
Brian Silah <unpervertedkid@gmail.com>
2023-09-20 15:53:08 +0300

    Attempting to use a moved value

--- rust_in_action/cube_sat/src/main.rs
+++ rust_in_action/cube_sat/src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug)]
+struct CubeSat {
+    id: u64
+}
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+fn check_status(cube_sat: CubeSat) -> StatusMessage {
+    return StatusMessage::Ok;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sat_a = CubeSat{ id: 0 };
+    let sat_b = CubeSat{ id: 1 };
+    let sat_c = CubeSat{ id: 2 };
+
+    let a_status = check_status(sat_a);
+  let b_status = check_status(sat_b);
+  let c_status = check_status(sat_c);
+  println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+
+  // "waiting" ...
+  let a_status = check_status(sat_a);
+  let b_status = check_status(sat_b);
+  let c_status = check_status(sat_c);
+  println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 }
