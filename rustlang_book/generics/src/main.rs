e411ae308e1ab0ad5b02df35f1dc2fb65602b8d4
Brian Silah <unpervertedkid@gmail.com>
2023-10-09 09:43:59 +0300

    Another function to find the largest char

--- rustlang_book/generics/src/main.rs
+++ rustlang_book/generics/src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
     let number_list = vec![34,57,87,64,98,100];
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
 
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    return largest;
+}
+
+fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for item in list {
