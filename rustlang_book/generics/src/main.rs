c99388b1dbf2ecee43266e6adc62059fb2985a09
Brian Silah <unpervertedkid@gmail.com>
2023-10-09 09:41:05 +0300

    Extracted duplicate code to a function.

--- rustlang_book/generics/src/main.rs
+++ rustlang_book/generics/src/main.rs
@@ -1,25 +1,23 @@
 fn main() {
     let number_list = vec![34,57,87,64,98,100];
 
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}", largest);
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let mut largest = &number_list[0];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
 
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
-    println!("The largest number is {}", largest);
+    return largest;
 }
