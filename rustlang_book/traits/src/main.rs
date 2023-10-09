f120e8db839ad0f98304c6e8ff50b7521a6a5af6
Brian Silah <unpervertedkid@gmail.com>
2023-10-09 10:15:43 +0300

    Using traits to defing common behavior

---
+++ rustlang_book/traits/src/main.rs
@@ -0,0 +1,49 @@
+pub trait Summary {
+    fn summarize(&self) -> String {
+        return String::from("Read more...");
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        return format!("{}: {}", self.username, self.content);
+    }
+}
+fn main() {
+    let tweet = Tweet {
+        username: String::from("just_a_kid_with_a_dream"),
+        content: String::from("You feel like home"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+}
