81ed8362ae2a76bc6e48420dc3b6b973df0b0ce1
Brian Silah <unpervertedkid@gmail.com>
2023-10-09 09:35:49 +0300

    Finding the largest number

---
+++ rustlang_book/generics/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let number_list = vec![34,57,87,64,98,100];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+}
