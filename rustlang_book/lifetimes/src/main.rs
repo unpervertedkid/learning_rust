4ca4f51a62612c86ff46cf5567b7e7e9bcc9f7a9
Brian Silah <unpervertedkid@gmail.com>
2023-10-17 10:48:16 +0300

    Lifetime annotations in struct definitions.

--- rustlang_book/lifetimes/src/main.rs
+++ rustlang_book/lifetimes/src/main.rs
@@ -1,9 +1,20 @@
+pub struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let novel = String::from("Once upon a time there existed a crab. He was called a rustacean...");
+    let first_sentence = novel.split('.').next().expect("Could not find a .");
+    let i = ImportantExcerpt {
+        part : first_sentence,
+    };
+    println!("The first sentence is: {}" , i.part); 
 }
 
 fn longest<'a>(string1: &'a str, string2: &'a str) -> &'a str {
