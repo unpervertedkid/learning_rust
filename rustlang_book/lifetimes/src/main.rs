003ec5680b10cd8fa88b053d2b347b7b5069a969
Brian Silah <unpervertedkid@gmail.com>
2023-10-17 10:41:05 +0300

    Using lifetime annotations to prevent dangling pointers

---
+++ rustlang_book/lifetimes/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(string1: &'a str, string2: &'a str) -> &'a str {
+    if string1.len() > string2.len() {
+        string1
+    } else {
+        string2
+    }
+}
