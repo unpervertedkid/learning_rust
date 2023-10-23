ed73638aef867694cfd778e40f80c754d3d026aa
Brian Silah <unpervertedkid@gmail.com>
2023-10-23 14:59:49 +0300

    Adding custom failure messages

--- rustlang_book/testing/adder/src/lib.rs
+++ rustlang_book/testing/adder/src/lib.rs
@@ -17,6 +17,10 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greet(name: &str) -> String {
+    format!("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,5 +72,14 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greet("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
     }
 }
