13f84f1e506561003b862fe4dfb06be230d2dcb0
Brian Silah <unpervertedkid@gmail.com>
2023-11-15 10:43:09 +0300

    Refactor string lines to be more clear

--- rustlang_book/minigrep/src/lib.rs
+++ rustlang_book/minigrep/src/lib.rs
@@ -76,10 +76,10 @@ mod tests {
     fn case_sensitive() {
         let query = "duct";
         let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
+                                Rust:
+                                safe, fast, productive.
+                                Pick three.
+                                Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
@@ -88,10 +88,10 @@ Duct tape.";
     fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
+                                Rust:
+                                safe, fast, productive.
+                                Pick three.
+                                Trust me.";
 
         assert_eq!(
             vec!["Rust:", "Trust me."],
