4a779359028b4f30ba8b8f60ef701cc0d940499c
Brian Silah <unpervertedkid@gmail.com>
2024-01-09 18:56:44 +0300

    feat: exercise 2!

--- rustlings/exercises/quiz2.rs
+++ rustlings/exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,27 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Append(number_of_times) => {
+                    let mut new_string = String::from(string);
+                    let mut count = 0;
+
+                    while count < *number_of_times {
+                        new_string.push_str("bar");
+                        count += 1;
+                    }
+
+                    output.push(new_string);
+                }
+                Command::Trim => output.push(string.trim().to_owned()),
+
+                Command::Uppercase => output.push(string.to_uppercase()),
+            }
         }
         output
     }
@@ -45,8 +59,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
