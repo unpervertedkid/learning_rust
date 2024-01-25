4ae10656ceb565ac47ae48d0c3a19371bb15f2eb
Brian Silah <unpervertedkid@gmail.com>
2024-01-25 11:56:17 +0300

    fix(Project Euler): simpler solution

--- project_euler/multiples_of_3_or_5/src/main.rs
+++ project_euler/multiples_of_3_or_5/src/main.rs
@@ -24,13 +24,20 @@ impl Iterator for MultiplesOf {
     }
 }
 fn main() {
+    // Naive trial
     let multiples_of_3_below_1000 = MultiplesOf::new(3).take_while(|multiple| multiple < &1000);
     let multiples_of_5_below_1000 = MultiplesOf::new(5).take_while(|multiple| multiple < &1000);
-    
-    let multiples_of_3_sum: i32 = multiples_of_3_below_1000.sum();
-    let  multiples_of_5_sum: i32 = multiples_of_5_below_1000.sum();
-    
-    let total_sum = multiples_of_3_sum + multiples_of_5_sum;
 
-    println!("The sum of multiples of 3 and 5 below 1000 is: {total_sum}")
+    let multiples_of_3_sum: i32 = multiples_of_3_below_1000.sum();
+    let multiples_of_5_sum: i32 = multiples_of_5_below_1000.sum();
+
+    // However, this is wrong as some numbers which are both multiples of 3 and 5 are summed twice
+    let _total_sum = multiples_of_3_sum + multiples_of_5_sum;
+
+    // Simple yet correct implementation
+    let multiples_of_3_or_5_below_1000 =
+        (1..1000).filter(|number| number % 3 == 0 || number % 5 == 0);
+
+    let total_sum: i32 = multiples_of_3_or_5_below_1000.sum();
+    println!("The sum of all multiples of 3 and 5 below 1000 is: {total_sum}");
 }
