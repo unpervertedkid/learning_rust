3a4081c79e0d9a7d3c9863f52532eff9fc2d93ee
Brian Silah <unpervertedkid@gmail.com>
2024-01-31 15:57:20 +0300

    feat(book): Creating a new thread with spawn

---
+++ rustlang_book/fearless_concurrency/threads/src/main.rs
@@ -0,0 +1,15 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    thread::spawn(|| {
+        for number in 1..10 {
+            println!("Hi number {} from the spawned thread!", number);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for number in 1..10 {
+        println!("Hi number {} from the main thread!", number);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
