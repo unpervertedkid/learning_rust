57e1a3a8af01005c57f2ad8dbeaaf430f23c64a0
briansilah <unpervertedkid@gmail.com>
2024-02-02 14:36:10 +0300

    feat(rustlang book): Creating multiple producers by cloning the transmitter

--- rustlang_book/fearless_concurrency/channels/src/main.rs
+++ rustlang_book/fearless_concurrency/channels/src/main.rs
@@ -3,9 +3,52 @@ use std::{sync::mpsc, thread, time::Duration};
 fn main() {
     separator();
     send_values_across_threads();
-    
+
     separator();
     send_multiple_values();
+
+    separator();
+    send_using_multiple_producers();
+}
+
+fn send_using_multiple_producers() {
+    let (transmitter, receiver) = mpsc::channel();
+
+    let transmitter_1 = transmitter;
+    let transmitter_2 = transmitter_1.clone();
+
+    thread::spawn(move || {
+        let values = vec![
+            String::from("Hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for value in values {
+            transmitter_1.send(value).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let values = vec![
+            String::from("More"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for value in values {
+            transmitter_2.send(value).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    
+
+    for received in receiver {
+        println!("Got: {received}");
+    }
 }
 
 fn send_values_across_threads() {
@@ -13,9 +56,9 @@ fn send_values_across_threads() {
 
     thread::spawn(move || {
         thread::sleep(Duration::from_nanos(1));
-        
+
         let value = String::from("Hello");
-        transmitter.send(value).unwrap();        
+        transmitter.send(value).unwrap();
     });
 
     loop {
@@ -29,8 +72,6 @@ fn send_values_across_threads() {
             println!("Doing other work.....");
         }
     }
-
-
 }
 
 fn send_multiple_values() {
