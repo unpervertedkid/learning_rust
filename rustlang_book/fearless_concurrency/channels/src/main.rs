db53befaa6b91af90853397a181d2ebaf7f8fb99
briansilah <unpervertedkid@gmail.com>
2024-02-02 14:28:06 +0300

    feat(rustlang book): Sending multiple values and seeing the receiver waiting

--- rustlang_book/fearless_concurrency/channels/src/main.rs
+++ rustlang_book/fearless_concurrency/channels/src/main.rs
@@ -1,13 +1,21 @@
 use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
+    separator();
+    send_values_across_threads();
+    
+    separator();
+    send_multiple_values();
+}
+
+fn send_values_across_threads() {
     let (transmitter, receiver) = mpsc::channel();
 
     thread::spawn(move || {
         thread::sleep(Duration::from_nanos(1));
-
+        
         let value = String::from("Hello");
-        transmitter.send(value).unwrap();
+        transmitter.send(value).unwrap();        
     });
 
     loop {
@@ -20,5 +28,33 @@ fn main() {
         } else {
             println!("Doing other work.....");
         }
-    }    
+    }
+
+
+}
+
+fn send_multiple_values() {
+    let (transmitter, receiver) = mpsc::channel();
+
+    thread::spawn(move || {
+        let values = vec![
+            String::from("Hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for value in values {
+            transmitter.send(value).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in receiver {
+        println!("Got: {received}");
+    }
+}
+
+fn separator() {
+    println!("------------------------------------------------------------");
 }
