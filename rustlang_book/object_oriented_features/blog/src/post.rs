c62d9f8c2a95b5bdd4a4c29068992757124161bd
briansilah <unpervertedkid@gmail.com>
2024-02-09 12:53:50 +0300

    feat(rustlang book): Object Oriented - State Pattern

---
+++ rustlang_book/object_oriented_features/blog/src/post.rs
@@ -0,0 +1,141 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Self {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.request_review())
+        }
+    }
+    pub fn approve(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.approve());
+        }
+    }
+
+    pub fn reject(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.reject());
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_draft_content() {
+        let mut post = Post::new();
+        post.add_text("Hello World");
+
+        assert_eq!("", post.content());
+    }
+
+    #[test]
+    fn test_pending_review() {
+        let mut post = Post::new();
+        post.add_text("Hello World");
+        post.request_review();
+
+        assert_eq!("", post.content());
+    }
+    
+    #[test]
+    fn test_approved_review() {
+        let mut post = Post::new();
+        post.add_text("Hello World");
+        post.request_review();
+        post.approve();
+
+        assert_eq!("Hello World", post.content());
+    }
+    
+    #[test]
+    fn test_rejected_review() {
+        let mut post = Post::new();
+        post.add_text("Hello World");
+        post.request_review();
+        post.reject();
+
+        assert_eq!("", post.content());
+    }
+}
